26b9ddd018e1ecf32197b4ba3f48b3b52c2ba1f8
Charles Villalpando <charlesvill@g.ucla.edu>
2023-11-16 13:00:56 -0800

    Removing test file

--- text.js
+++
@@ -1,8 +0,0 @@
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-
-function removeDuplicates(arr) { 
-	return arr.filter((item, 
-		index) => arr.indexOf(item) === index); 
-} 
-console.log(removeDuplicates(arr));
-
